db7858012acca16df97921d07a771a0112fc9d21
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-09-21 23:34:19 +0300

    Addition() refactoring + explanation. Multiplication() code removed.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,5 +4,5 @@ import StringMultiplication.{Addition, Multiplication}
   * Created by Robert-PC on 9/21/2017.
   */
 object Main extends App{
-  println(Addition("4999", "51"))
+  println(Addition("4", "59"))
 }
--- src/main/scala/StringMultiplication/Addition.scala
+++ src/main/scala/StringMultiplication/Addition.scala
@@ -4,18 +4,29 @@ package StringMultiplication
   * Created by Robert-PC on 9/21/2017.
   */
 case class Total(sum: String = "", carry: Int = 0)
+/*
+    The algorithm is rather simple:
+    1. both strings become of same size by making the both strings of same size ( so there arent any losses) , using the neutral
+        element. Ex: "1", "1000" => "0001", "1000"
+    2. match every digit with its corresponding digit
+    3. for every tuple, add the digits, saving the sum and the carry.
+      Ex: (9,9) => (1, 8) => 1 is carry, 8 is sum.
+    4. Folding from right to left the result -> the accumulator keeps track of the final sum and the carry.
+
+ */
+
 
 object Addition {
   def apply(x: String, y: String): String = {
-    val resultWithoutCarry = equalize(x, y).zip(equalize(y, x))
+    val resultWithoutCarry = equalizeLength(x, y).zip(equalizeLength(y, x))
       .map(e => (
         getCarry(e._1, e._2),
         getCurrent(e._1, e._2)
       ))
-      .foldRight(new Total())((current, total) =>
+      .foldRight(Total())((curr, acc) =>
         Total(
-          ((current._2 + total.carry) % 10).toString ++ total.sum,
-          (current._2 + total.carry) / 10 + current._1
+          updateSum(curr._2, acc),
+          updateCarry(curr, acc)
         )
       )
 
@@ -25,6 +36,10 @@ object Addition {
       resultWithoutCarry.sum
   }
 
+  private def updateSum(curr: Int, acc: Total): String = ((curr + acc.carry) % 10).toString ++ acc.sum
+
+  private def updateCarry(curr: (Int, Int), acc: Total): Int = (curr._2 + acc.carry) / 10 + curr._1
+
   private def getCurrent(x: Char, y: Char): Int = {
     (x.asDigit + y.asDigit) % 10
   }
@@ -33,7 +48,7 @@ object Addition {
     (x.asDigit + y.asDigit) / 10
   }
 
-  private def equalize(first: String, second: String): String = {
+  private def equalizeLength(first: String, second: String): String = {
     if (first.length >= second.length)
       first
     else
