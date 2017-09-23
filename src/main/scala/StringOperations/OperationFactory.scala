95778702a3ca4726c6505b43d2c7b9dad1532df8
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-09-23 22:57:15 +0300

    Refactoring OperationFactory.

--- src/main/scala/StringOperations/OperationFactory.scala
+++ src/main/scala/StringOperations/OperationFactory.scala
@@ -5,12 +5,20 @@ package StringOperations
   */
 trait OperationFactory {
   def compute(x: String, y: String, operation: Operation): Option[String] = {
-    operation match {
-      case Add      => Addition(x, y)
-      case Multiply => Multiplication(x, y)
-      case Subtract => Subtraction(x, y)
-      case Pow      => Exponentiation(x, y)
-      case _        => None
+     def executeComputation(x: String, y: String, operation: Operation): Option[String] = {
+      operation match {
+        case Add      => Addition(x, y)
+        case Multiply => Multiplication(x, y)
+        case Subtract => Subtraction(x, y)
+        case Pow      => Exponentiation(x, y)
+        case _        => None
+      }
     }
+
+    executeComputation(x, y, operation)
+  }
+
+  private def isValid(x: String, y: String): Boolean = {
+    x.forall(_.isDigit) && y.forall(_.isDigit)
   }
 }
