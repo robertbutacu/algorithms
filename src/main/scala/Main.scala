fa23ffcbadf2d98a5270106a301c3b8185d55aa0
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-09-24 17:30:12 +0300

    Division is highly inefficient :/

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,5 +4,5 @@ import StringOperations._
   * Created by Robert-PC on 9/21/2017.
   */
 object Main extends App with OperationFactory{
-  println(compute(Some("2"), Add, Some("-10")))
+  println(compute(Some("21231231412352312312"), Divide, Some("11231231231231230")))
 }
