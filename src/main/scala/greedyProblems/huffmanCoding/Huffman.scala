25c9204a6ebb7d46ab7450a88e644f843994cc43
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-09-28 20:50:33 +0300

    createCodeTree() implemented.

--- src/main/scala/greedyProblems/huffmanCoding/Huffman.scala
+++ src/main/scala/greedyProblems/huffmanCoding/Huffman.scala
@@ -150,8 +150,7 @@ object Huffman {
     * The parameter `chars` is an arbitrary text. This function extracts the character
     * frequencies from that text and creates a code tree based on them.
     */
-  //def createCodeTree(chars: List[Char]): CodeTree
-
+  def createCodeTree(chars: List[Char]): CodeTree = until(singleton, combine)(makeOrderedLeafList(times(chars))).head
 
   // Part 3: Decoding
 
@@ -161,9 +160,9 @@ object Huffman {
     * This function decodes the bit sequence `bits` using the code tree `tree` and returns
     * the resulting list of characters.
     */
-  //def decode(tree: CodeTree, bits: List[Bit]): List[Char] = {
-   // def traverse(remaining: CodeTree, bits: List[Bit]): List[Char]
-  //}
+  def decode(tree: CodeTree, bits: List[Bit]): List[Char] = {
+    def traverse(remaining: CodeTree, bits: List[Bit]): List[Char]
+  }
 
   /**
     * A Huffman coding tree for the French language.
