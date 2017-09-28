a205d3bb8deb0242c970c1a4464ab17767c18c3b
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-09-28 21:45:19 +0300

    Added number transformation to a list of `bits`

--- src/main/scala/fastExponentiation/FastExponentiation.scala
+++ src/main/scala/fastExponentiation/FastExponentiation.scala
@@ -11,4 +11,16 @@ object FastExponentiation {
 
   lazy val powers: Stream[Int] = Stream.cons(1,Stream.from(2, 2).filter(isPowerOf2))
 
+  def toBits(input: Int): List[Char] = {
+    powers.takeWhile(_ <= input)
+      .reverse
+      .toList
+      .foldLeft("", input)((acc, curr) =>
+      if(acc._2 - curr >= 0) (acc._1 ++ "1", acc._2 - curr)
+      else                   (acc._1 ++ "0", acc._2)
+    )
+      ._1
+      .toList
+  }
+
 }
