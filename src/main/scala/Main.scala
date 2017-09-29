c8b774345a78032ccadb552d13f9b7e57f5c51c5
robertbutacu <robert.butacu@yahoo.com>
2017-09-29 18:51:15 +0300

    generatePowersOf2() works as intended

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,12 +2,12 @@ import fastExponentiation.FastExponentiation
 import greedyProblems.huffmanCoding.Huffman
 import stringOperations._
 import stringOperations.examples.StreamsExamples
-import stringOperations.operations.Mod
+import stringOperations.operations.{FastExp, Mod}
 import stringOperations.utils._
 
 /**
   * Created by Robert-PC on 9/21/2017.
   */
 object Main extends App with OperationFactory with StreamsExamples{
-  println(squaresUpUntil(Pos("10")).toList)
+  println(FastExp.generatePowersOf2("1", "100", List("0", "1")))
 }
--- src/main/scala/stringOperations/operations/FastExp.scala
+++ src/main/scala/stringOperations/operations/FastExp.scala
@@ -6,8 +6,14 @@ object FastExp {
     ""
   }
 
-  private def generatePowersOf2(start: String, goal: String): List[Char] = {
-    List()
+  def increment(curr: String, goal: String, result: List[String]): List[String] = {
+    if(Mul(result.last, "2") == curr) generatePowersOf2(Inc(curr), goal, result ::: List(curr))
+    else generatePowersOf2(Inc(curr), goal, result)
+  }
+
+  def generatePowersOf2(start: String, goal: String, result: List[String]): List[String] = {
+    if(isBigger(start, goal)) result
+    else increment(start, goal, result)
   }
 
   def computePowersRightToLeft(base: String, power: List[Char]): List[Int] = {
@@ -15,4 +21,9 @@ object FastExp {
   }
 
   def toBits(input: String): List[Char] = List()
+
+  private def isBigger(x: String, y: String): Boolean = {
+    if (x.length > y.length || (x.length == y.length && x > y)) true
+    else false
+  }
 }
