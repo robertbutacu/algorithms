b800d85d176721e4b2b962fa6f9b6831430a1956
robertbutacu <robert.butacu@yahoo.com>
2017-09-30 19:00:26 +0300

    Renaming on time() method.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,13 +7,13 @@ import stringOperations.utils._
   */
 object Main extends App with OperationFactory with StreamsExamples{
 
-  def time[R](block: => R, sortingMethod: String): R = {
+  def time[R](block: => R, methodName: String): R = {
     val t0 = System.currentTimeMillis()
     val result = block // call-by-name
     val t1 = System.currentTimeMillis()
-    println(s"Elapsed time on $sortingMethod: " + (t1 - t0) + "ms")
+    println(s"Elapsed time on $methodName: " + (t1 - t0) + "ms")
     result
   }
-  println(compute(Decrement, Some(Neg("0"))))
-  //println(compute(Some(Neg("2")), Pow, Some(Pos("1000"))))
+  //println(compute(Decrement, Some(Pos())))
+  time(compute(Some(Pos("2")), Pow, Some(Pos("1000"))),"Fast exp")
 }
