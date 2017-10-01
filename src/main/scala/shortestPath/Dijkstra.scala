c44e39f85010e932b222414be3ae6b998b010803
robertbutacu <robert.butacu@yahoo.com>
2017-10-01 16:30:44 +0300

    Refactored shortest() to receive less parameters and call a recursive function

--- src/main/scala/shortestPath/Dijkstra.scala
+++ src/main/scala/shortestPath/Dijkstra.scala
@@ -15,14 +15,21 @@ object Dijkstra {
     graph.nodes map {node: Node => if(node == start) TentativeDistance(node, Some(0)) else TentativeDistance(node, None)}
   }
 
-  //@tailrec
-  def shortest(curr: Node,
+  def shortest(start: Node,
                goalNode: Node,
-               tentDist: Set[TentativeDistance],
-               graph: Graph,
-               visited: Set[Node],
-               path: Set[Node]): Set[Node] = {
-    Set()
+               graph: Graph): Set[Node] = {
+    //@tailrec
+    def go(curr: Node, goalNode: Node, tentDist: Set[TentativeDistance], visited: Set[Node], path: Set[Node]): Set[Node] = {
+      Set()
+    }
+
+    go(
+      start,
+      goalNode,
+      Set.empty[TentativeDistance],
+      Set.empty[Node],
+      Set.empty[Node]
+    )
   }
 
   def removeTentativeNode(replacement: TentativeDistance, tentNodes: Set[TentativeDistance]): Set[TentativeDistance] = {
