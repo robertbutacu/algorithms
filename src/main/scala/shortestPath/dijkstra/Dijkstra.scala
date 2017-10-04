aacc3291f1329651411214789b7cb7eaab313390
robertbutacu <robert.butacu@yahoo.com>
2017-10-05 01:23:34 +0300

    Works - still need to refactor so it return the list of nodes.

--- src/main/scala/shortestPath/dijkstra/Dijkstra.scala
+++ src/main/scala/shortestPath/dijkstra/Dijkstra.scala
@@ -6,7 +6,7 @@ import scala.collection.mutable
 /**
   * Created by Robert-PC on 9/21/2017.
   */
-object Dijkstra extends GraphExample{
+object Dijkstra extends GraphExample {
   type Graph = List[Node]
   type Distance = Int
   type Edges = List[(Node, Distance)]
@@ -17,31 +17,31 @@ object Dijkstra extends GraphExample{
 
     //TODO nodes that aren't visited are put multiple times in the priority queue
     def go(curr: Node, goalNode: Node, priorityQueue: mutable.MutableList[Node], visited: Set[Node]): Int = {
-      if(curr.name == goalNode.name)
-        100
-      else{
-        Thread.sleep(3000)
-        println("Current " + curr.name)
-        val visitedUpdated = visited ++ Set(curr)
+      Thread.sleep(3000)
+      println("Current " + curr.name)
+      val visitedUpdated = visited ++ Set(curr)
 
-        //updating tentative distances
-        curr.updateNeighborsTentativeDistances(visitedUpdated)
+      //updating tentative distances
+      curr.updateNeighborsTentativeDistances(visitedUpdated)
 
-        //update priority queueQ
-        curr.neighbors map (_._1) filterNot visitedUpdated.contains foreach (node => if(!priorityQueue.contains(node)) priorityQueue += node)
+      //update priority queueQ
+      curr.neighbors map (_._1) filterNot visitedUpdated.contains foreach (node => if (!priorityQueue.contains(node)) priorityQueue += node)
 
-        //removing current from priority list
-        val updatedPq = priorityQueue filterNot (_ == curr)
+      //removing current from priority list
+      val updatedPq = priorityQueue filterNot (_ == curr)
 
-        //re-sorting the queue
-        updatedPq sortWith (_.tentativeDistance < _.tentativeDistance)
+      //re-sorting the queue
+      updatedPq sortWith (_.tentativeDistance < _.tentativeDistance)
+
+      if (updatedPq.isEmpty)
+        goalNode.tentativeDistance
+      else
         go(
           updatedPq.head,
           goalNode,
           updatedPq,
           visitedUpdated
         )
-      }
     }
 
     go(
