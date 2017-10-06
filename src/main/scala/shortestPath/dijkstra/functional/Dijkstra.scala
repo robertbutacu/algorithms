65aa39df2bc3e7df7ba6fb49e7ef74f5332e2389
robertbutacu <robert.butacu@yahoo.com>
2017-10-06 23:38:09 +0300

    more Types added basic scaffolding of the algorithm

--- src/main/scala/shortestPath/dijkstra/functional/Dijkstra.scala
+++ src/main/scala/shortestPath/dijkstra/functional/Dijkstra.scala
@@ -1,10 +1,34 @@
 package shortestPath.dijkstra.functional
 
+import scala.annotation.tailrec
+
 object Dijkstra {
-  type CityName = String
-  type Distance = Int
-  type Graph    = Map[Edge, Distance]
-  case class Node(name: CityName, tentativeDistance : Option[Distance] = None)
+  type CityName      = String
+  type Distance      = Int
+  type Graph         = Map[Edge, Distance]
+  type Path          = (List[Edge], Distance)
+  type VisitedNodes  = List[Node]
+  type PriorityQueue = List[Node]
+  case class Node(name: CityName, tentativeDistance : Option[Distance] = None, previous: Option[Node] = None)
   case class Edge(from: Node, to: Node)
+
+  def shortest(from: Node, to: Node, graph: Graph): Path = {
+    //@tailrec
+    def go(curr: Node, pq: PriorityQueue, vn: VisitedNodes): Path = {
+      (List(), 0)
+      /**
+        * add curr to visited list
+        *
+        * updated current node's neighbors
+        *
+        * update priority queue
+        *
+        * recursive call to go until curr is equal to to
+        */
+    }
+
+    go(from, List(), List())
+  }
+
 
 }
