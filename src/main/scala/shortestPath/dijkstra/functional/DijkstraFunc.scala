1a8d21fde53ebb50377e44671005ad7a84521834
robertbutacu <robert.butacu@yahoo.com>
2017-10-07 17:05:33 +0300

    syntactic sugar and indentation

--- src/main/scala/shortestPath/dijkstra/functional/DijkstraFunc.scala
+++ src/main/scala/shortestPath/dijkstra/functional/DijkstraFunc.scala
@@ -5,29 +5,29 @@ import scala.annotation.tailrec
 /**
   * Created by Robert-PC on 9/21/2017.
   *
-  *   Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph, which may represent, for example, road networks.
+  * Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph, which may represent, for example, road networks.
   * It works only on graphs with positive weighted edges.
   *
-  *   In order to assure immutability as easy as possible, the graph itself is represented as a list of Edges represented by 2 IDs of nodes,
+  * In order to assure immutability as easy as possible, the graph itself is represented as a list of Edges represented by 2 IDs of nodes,
   * instead of the nodes itself, and the nodes are stored in a list.
-  *   This way, if a tentative distance is to be updated, it will updated in a single place, not for every appearance of the node in the graph.
-  *   -> true for prevNode also.
-  *   Thus, it is easier to keep track of the state of the nodes, and also to output the final path to the destination node.
+  * This way, if a tentative distance is to be updated, it will updated in a single place, not for every appearance of the node in the graph.
+  * -> true for prevNode also.
+  * Thus, it is easier to keep track of the state of the nodes, and also to output the final path to the destination node.
   */
 
 object DijkstraFunc {
-  type CityName      = String
-  type Distance      = Int
-  type NodeId        = Int
-  type Graph         = Map[Edge, Distance]
-  type Path          = (Nodes, Distance)
-  type VisitedNodes  = Nodes
+  type CityName = String
+  type Distance = Int
+  type NodeId = Int
+  type Graph = Map[Edge, Distance]
+  type Path = (Nodes, Distance)
+  type VisitedNodes = Nodes
   type PriorityQueue = List[Node]
-  type Nodes         = Set[Node]
+  type Nodes = Set[Node]
 
   case class Node(id: NodeId,
                   name: CityName,
-                  tentativeDistance : Distance = Int.MaxValue,
+                  tentativeDistance: Distance = Int.MaxValue,
                   previous: Option[Node] = None)
 
   case class Edge(from: NodeId, to: NodeId)
@@ -35,9 +35,9 @@ object DijkstraFunc {
   def shortest(from: Node, to: Node, nodes: Nodes, graph: Graph): Path = {
     @tailrec
     def go(curr: Node, nodes: Nodes, pq: PriorityQueue, vn: VisitedNodes): Path = {
-      if(curr.name == to.name)
+      if (curr.name == to.name)
         (path(curr) + curr, curr.tentativeDistance)
-      else{
+      else {
         println("Currently in " + curr.name + " " + curr.tentativeDistance)
 
         val neighbors = getNeighbors(curr, graph)
@@ -47,9 +47,9 @@ object DijkstraFunc {
         val updatedNodes = transformNeighbors(curr, neighbors, nodes)
 
         val updatedPq = transformPriorityQueue(
-          updatedNodes.filter(n => neighbors.keys.toList.contains(n.id)),
+          updatedNodes filter (n => neighbors.keys.toList.contains(n.id)),
           updatedVn,
-          pq filterNot(_.name == curr.name))
+          pq filterNot (_.name == curr.name))
 
         go(updatedPq.head,
           updatedNodes,
@@ -78,7 +78,7 @@ object DijkstraFunc {
   }
 
   def getNeighbors(node: Node, graph: Graph): Map[NodeId, Distance] = {
-    graph filterKeys(e => e.from == node.id) map(c => (c._1.to, c._2))
+    graph filterKeys (e => e.from == node.id) map (c => (c._1.to, c._2))
   }
 
   def orderPriorityQueue(priorityQueue: PriorityQueue): PriorityQueue = {
