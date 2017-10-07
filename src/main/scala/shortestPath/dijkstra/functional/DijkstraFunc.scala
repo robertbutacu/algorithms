68059745f6185e84f0df14dc73f3b84f1b6e9375
robertbutacu <robert.butacu@yahoo.com>
2017-10-07 10:13:52 +0300

    refactored functional dijkstra

--- src/main/scala/shortestPath/dijkstra/functional/DijkstraFunc.scala
+++ src/main/scala/shortestPath/dijkstra/functional/DijkstraFunc.scala
@@ -1,19 +1,26 @@
 package shortestPath.dijkstra.functional
 
+import java.util.UUID
+
 import scala.annotation.tailrec
 
 object DijkstraFunc {
   type CityName      = String
   type Distance      = Int
-  type Graph         = Map[Edge, Distance]
+  type NodeId        = Int
+  type Graph         = Map[NodeId, Distance]
   type Path          = (List[Edge], Distance)
-  type VisitedNodes  = List[Node]
-  type PriorityQueue = List[Node]
-  case class Node(name: CityName, tentativeDistance : Distance = Int.MaxValue, previous: Option[Node] = None)
-  case class Edge(from: Node, to: Node)
+  type VisitedNodes  = List[NodeId]
+  type PriorityQueue = Nodes
+  type Nodes         = List[Node]
+  case class Node(id: NodeId = UUID.randomUUID().toString.toInt,
+                  name: CityName,
+                  tentativeDistance : Distance = Int.MaxValue,
+                  previous: Option[Node] = None)
+  case class Edge(from: NodeId, to: NodeId)
 
   def shortest(from: Node, to: Node, graph: Graph): Path = {
-    @tailrec
+    //@tailrec
     def go(curr: Node, currGraph: Graph, pq: PriorityQueue, vn: VisitedNodes): Path = {
       /**
         * add curr to visited list
@@ -24,17 +31,7 @@ object DijkstraFunc {
         *
         * recursive call to go until curr is equal to to
         */
-      val updatedVn = pq :+ curr
-
-      val updatedGraph = transformNeighbors(currGraph)
-
-      val updatedPq = transformPriorityQueue(
-        (updatedGraph filterKeys(e => e.from.name == curr.name) map (r => r._1.to)).toList,
-        updatedVn,
-        pq)
-
-      if(curr.name == to.name) (List(), curr.tentativeDistance)
-      else go(updatedPq.head, updatedGraph, updatedPq, updatedVn)
+      (List(), 0)
     }
 
     go(from, graph, List(), List())
@@ -42,21 +39,19 @@ object DijkstraFunc {
 
   //def path(goal: Node, graph: Graph)
 
-  def transformNeighbors(graph: Graph): Graph = {
-    graph.map(r => (transformEdge(r._1, r._2), r._2))
+  def transformNeighbors(graph: Graph, nodes: Nodes): Graph = {
+    Map[NodeId, Distance]().empty
   }
 
   def transformEdge(edge: Edge, distance: Distance): Edge = {
-    val tentDist = edge.from.tentativeDistance + distance
-    if(tentDist < edge.to.tentativeDistance) Edge(edge.from, Node(edge.to.name, tentDist, Some(edge.from)))
-    else                                     edge
+    Edge(0 , 0)
   }
 
   def orderPriorityQueue(priorityQueue: PriorityQueue): PriorityQueue = {
-    priorityQueue sortWith (_.tentativeDistance < _.tentativeDistance)
+    List()
   }
 
   def transformPriorityQueue(nodes: List[Node], visitedNodes: VisitedNodes, priorityQueue: PriorityQueue): PriorityQueue = {
-    orderPriorityQueue(priorityQueue ++ nodes filterNot visitedNodes.contains filterNot priorityQueue.contains)
+    List()
   }
 }
